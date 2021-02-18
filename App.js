429754ce1ed08702ccf0f556eefc4dcb96e66706
GUNEY <guney@veribis.com.tr>
2021-02-18 23:04:45 +0300

    Touchables

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, StyleSheet, Text, SafeAreaView, Alert, Button } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => Alert.alert("Opacity Clicked")
+  const active = false;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Touchables</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePress} >
+        <Text style={styles.text}>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight style={styles.button} underlayColor="#345212" onPress={handlePress}>
+        <Text style={styles.text}>TouchableHighlight</Text>
+      </TouchableHighlight>
+      <TouchableWithoutFeedback onPress={handlePress}>
+        <Text style={{ backgroundColor: active ? 'brown' : 'red' }}>TouchableWithoutFeedback</Text>
+      </TouchableWithoutFeedback>
+
+    </SafeAreaView >
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  button: {
+    backgroundColor: "pink",
+    height: 80,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+    borderRadius: 10
+
+  },
+  text: {
+    fontSize: 18,
+    color: 'brown',
+    fontWeight: "normal"
+  }
 });
