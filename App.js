8f6a39b4ca070ff0a5098bb553414c28d86f54c6
GUNEY <guney@veribis.com.tr>
2021-02-20 23:28:55 +0300

    Image and ImageBackground

--- App.js
+++ App.js
@@ -1,48 +1,28 @@
 import React from 'react';
-import { TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, StyleSheet, Text, SafeAreaView, Alert, Button } from 'react-native';
+import { ActivityIndicator, Image, ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
+import screenImage from './assets/image.jpeg';
 
-export default function App() {
+const App = () => {
 
-  const handlePress = () => Alert.alert("Opacity Clicked")
-  const active = false;
-
+  const isLoading = false;
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Touchables</Text>
-      <TouchableOpacity style={styles.button} onPress={handlePress} >
-        <Text style={styles.text}>TouchableOpacity</Text>
-      </TouchableOpacity>
-      <TouchableHighlight style={styles.button} underlayColor="#345212" onPress={handlePress}>
-        <Text style={styles.text}>TouchableHighlight</Text>
-      </TouchableHighlight>
-      <TouchableWithoutFeedback onPress={handlePress}>
-        <Text style={{ backgroundColor: active ? 'brown' : 'red' }}>TouchableWithoutFeedback</Text>
-      </TouchableWithoutFeedback>
+    <View style={styles.container}>
+      {
+        isLoading && <ActivityIndicator color="blue" size="large" />
+      }
 
-    </SafeAreaView >
-
+    </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  },
-  button: {
-    backgroundColor: "pink",
-    height: 80,
-    width: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginVertical: 10,
-    borderRadius: 10
 
   },
-  text: {
-    fontSize: 18,
-    color: 'brown',
-    fontWeight: "normal"
-  }
+
 });
---
+++ Touchables.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import { TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, StyleSheet, Text, SafeAreaView, Alert, Button } from 'react-native';
+
+const Touchables = () => {
+  const handlePress = () => Alert.alert("Opacity Clicked")
+  const active = false;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Touchables</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePress} >
+        <Text style={styles.text}>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight style={styles.button} underlayColor="#345212" onPress={handlePress}>
+        <Text style={styles.text}>TouchableHighlight</Text>
+      </TouchableHighlight>
+      <TouchableWithoutFeedback onPress={handlePress}>
+        <Text style={{ backgroundColor: active ? 'brown' : 'red' }}>TouchableWithoutFeedback</Text>
+      </TouchableWithoutFeedback>
+
+    </SafeAreaView >
+}
+
+export default Touchables
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: "pink",
+    height: 80,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+    borderRadius: 10
+
+  },
+  text: {
+    fontSize: 18,
+    color: 'brown',
+    fontWeight: "normal"
+  }
+})
+
